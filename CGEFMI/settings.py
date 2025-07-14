f0caa064c0283f4a1173a5a454ce4c90ee6e4889
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2025-07-14 15:56:10 +0100

    Update settings.py

--- CGEFMI/settings.py
+++ CGEFMI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7jt2hks+6!*v5fnpyxj--6on8z%6&n4*$_m4t4sh6qkue@_!@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
